094d8f099989a623d40f9abf9e845a407b7984ef
Heesoo Kim <heesoo0323@gmail.com>
2021-06-28 10:49:04 -0700

    Completed javacript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+var fullName = 'Heesoo Shane Kim';
+console.log('value of fullName', fullName);
+console.log('typeof fullName', typeof 'Heesoo Shane Kim');
+
+var isCool = true;
+Boolean(isCool);
+console.log('value of isCool', isCool);
+console.log('typeof isCool', typeof true);
+
+var TotalPets = 0;
+console.log('value of TotalPets', TotalPets);
+console.log('typeof TotalPets', typeof 0);
+
+var unknown = null;
+console.log('value of unknown', unknown);
+console.log('typeof unknown', typeof null);
+
+var never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof undefined);
