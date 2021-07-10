8d27fb4d29272cdc12b88ccd868df595c5bcb188
Heesoo Kim <heesoo0323@gmail.com>
2021-07-09 17:50:19 -0700

    rough draft 1.

---
+++ javascript-and-json/main.js
@@ -0,0 +1,19 @@
+var newArray = [
+  {
+    isbn: '753-4553-22332',
+    title: 'And Then There Were None',
+    author: 'Agatha Christie'
+  },
+  {
+    isbn: '523-5443-45443',
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien'
+  },
+  {
+    isbn: '23-3443-234453',
+    title: 'Alice in Wonderland',
+    author: 'Lewis Carroll'
+  }
+];
+console.log('book information:', newArray);
+console.log('Type of newArray:', typeof newArray);
