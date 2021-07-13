e844101e850b247f1862185465ecf443574e37dd
Heesoo Kim <heesoo0323@gmail.com>
2021-07-13 09:00:52 -0700

    Completed javascript-typing tutor.

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,16 @@
+var $span = document.querySelectorAll('span');
+document.addEventListener('keydown', keyHandler);
+var i = 0;
+
+function keyHandler(event) {
+  $span[i].className = 'underline';
+  if (i === ($span.length - 1)) {
+    $span[i].className = 'right';
+  } else if (event.key === $span[i].textContent) {
+    $span[i].className = 'right';
+    $span[i + 1].className = 'underline';
+    i++;
+  } else {
+    $span[i].className = 'wrong underline';
+  }
+}
