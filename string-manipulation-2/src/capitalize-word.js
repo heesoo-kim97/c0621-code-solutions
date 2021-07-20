7f8e328d2a6b7945df98e6cdb3bc16c12894e438
Heesoo Kim <heesoo0323@gmail.com>
2021-07-19 17:58:19 -0700

    worked on capitalize word.

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,18 @@
 /* exported capitalizeWord */
+// function capitalizeWord(word) {
+//   var array = [];
+//   for (var i = 0; i < word.length; i++) {
+//     if (i === 0) {
+//       var firstLetter = word[0].toUpperCase();
+//       array.push(firstLetter);
+//     } else {
+//       var noCapital = word.toLowerCase();
+//       array.push(noCapital);
+//     }
+//   }
+//   return array;
+// }
+// if
+//   var captialize = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+//   return captialize;
+// }
