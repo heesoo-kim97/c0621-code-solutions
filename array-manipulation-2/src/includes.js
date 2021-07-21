ff9b36310ff1871f9dfcae13f8bb875b478f4434
Heesoo Kim <heesoo0323@gmail.com>
2021-07-20 18:02:05 -0700

    almost done with array-manipulation-2.

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,10 @@
 /* exported includes */
+function includes(array, value) {
+  var boolean = false;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true;
+    }
+  }
+  return boolean;
+}
--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,11 @@
 /* exported takeRight */
+function takeRight(array, count) {
+  var newArray = [];
+  for (var i = array.length - count; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  if (count > array.length) {
+    newArray = array;
+  }
+  return newArray;
+}
