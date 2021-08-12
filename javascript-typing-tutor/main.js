f10bc084ba233c78ad6219c18ffb8e8f6484b9b9
heesoo-kim97 <heesoo0323@gmail.com>
2021-08-12 21:51:38 +0000

    added return statement instead of classname.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -5,7 +5,7 @@ var i = 0;
 function keyHandler(event) {
   $span[i].className = 'underline';
   if (i === ($span.length)) {
-    $span[i].className = 'right';
+    return null;
   } else if (event.key === $span[i].textContent) {
     $span[i].className = 'right';
     $span[i + 1].className = 'underline';
