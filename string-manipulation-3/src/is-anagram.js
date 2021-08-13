c6a725ec7124a86c635c038901eb258d15c2c129
heesoo-kim97 <heesoo0323@gmail.com>
2021-08-13 01:00:47 +0000

    finished is-palindromic.js and started is-anagram.js.

---
+++ string-manipulation-3/src/is-anagram.js
@@ -0,0 +1,9 @@
+/* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  // var track = 0;
+  // for (var i = 0; i < firstString.length; i++) {
+  //   if (firstString[i] === secondString[track]) {
+
+  //   }
+  // }
+}
