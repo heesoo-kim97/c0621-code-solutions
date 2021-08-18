3eccf2c353d39eb3ac54fb814efa578d78e18f66
heesoo-kim97 <heesoo0323@gmail.com>
2021-08-18 00:54:56 +0000

    Finished is-anagram.js exercise.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,9 +1,31 @@
 /* exported isAnagram */
 function isAnagram(firstString, secondString) {
-  // var track = 0;
-  // for (var i = 0; i < firstString.length; i++) {
-  //   if (firstString[i] === secondString[track]) {
+  var newString1 = '';
+  var newString2 = '';
+  for (var i = 0; i < firstString.length; i++) {
+    if (firstString[i] !== ' ') {
+      newString1 += firstString[i];
+    }
+  }
+  for (var j = 0; j < secondString.length; j++) {
+    if (secondString[j] !== ' ') {
+      newString2 += secondString[j];
+    }
+  }
 
-  //   }
-  // }
+  var anagram = newString2;
+
+  for (var k = 0; k < newString1.length; k++) {
+    for (var l = 0; l < newString2.length; l++) {
+      if (newString1[k] === newString2[l]) {
+        anagram = anagram.replace(newString1[k], '');
+        break;
+      }
+    }
+  }
+  if (anagram === '') {
+    return true;
+  } else {
+    return false;
+  }
 }
