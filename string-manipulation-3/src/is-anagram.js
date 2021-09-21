f6472827695e466fdb77a058c85b67f2a7841780
heesoo-kim97 <heesoo0323@gmail.com>
2021-09-21 07:44:53 +0000

    Completed string-manipulation-3.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -29,3 +29,13 @@ function isAnagram(firstString, secondString) {
     return false;
   }
 }
+
+// 1. First define the function with two parameters
+// 2. Define two variables that can carry empty string for each
+// 3. using for loops, we can loop through firstString parameter while also
+// cycling through secondString parameter.
+// 4. If the letters match since anagrams have same letters with same order, we can report the match
+// 5. To account for repeated characters, I assign the variable that holds previous character to new variable
+// 6. We loop through both orginal two variables to check if specific letter was found in other word
+// 7. We can 'cross off' when the characters match and see if it is truly an anagram
+// 8. return boolean value to show whether the two parameters are indeed anagrams
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,41 @@
 /* exported titleCase */
+
+function titleCase(string) {
+  var minorWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+  string = string.split(' ');
+  for (var i = 0; i < string.length; i++) {
+
+    if (string[i] === 'api') {
+      string[i] = string[i].toUpperCase();
+    }
+    if (string[i].toLowerCase().includes('javascript')) {
+      if (string[i].includes(':')) {
+        string[i] = 'JavaScript:';
+      } else {
+        string[i] = 'JavaScript';
+      }
+    }
+    if (!minorWords.includes(string[i]) || string[i] === string[0]) {
+      string[i] = string[i][0].toUpperCase() + string[i].substring(1);
+    }
+    if (string[i].includes(':')) {
+      string[i + 1] = string[i + 1][0].toUpperCase() + string[i + 1].substring(1);
+    }
+    if (string[i].includes('-')) {
+      var newString = string[i].split('-');
+      newString[1] = newString[1][0].toUpperCase() + newString[1].substring(1);
+      newString = newString.join('-');
+      string[i] = newString;
+    }
+  }
+  string = string.join(' ');
+  return string;
+}
+
+// Define variables that has empty array and empty string
+// I would need to capitalize each first letter of each word by looping through the parameter split string value
+// I would need to filter "minor" words with if statement
+// I would need loop throught the word and check for '-' using if statment and captalize the next character
+// loop through the string parameter once again and filter using if statement to captailize 'api'
+// While looping through the string, captialize the S and the next character after 'Javascript:'
+// Returned the joined string value
