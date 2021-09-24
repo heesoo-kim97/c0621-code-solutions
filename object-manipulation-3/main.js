33c1ed89ecdd68f27d261464934196118e57c022
heesoo-kim97 <heesoo0323@gmail.com>
2021-09-24 23:59:04 +0000

    initiaized varaibles that holds array of data.

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,11 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+const player1 = { name: 'Issac', hand: [] };
+const player2 = { name: 'Heesoo', hand: [] };
+const player3 = { name: 'Denny', hand: [] };
+const player4 = { name: 'Paul', hand: [] };
+
+const suits = ['heart', 'diamond', 'spades', 'clubs'];
+const rank = ['Ace', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+
+console.log(player1, player2, player3, player4, suits, rank);
