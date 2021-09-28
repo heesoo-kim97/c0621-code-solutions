0adb17748a091b1d4db3935c66d5a1fa457c1316
heesoo-kim97 <heesoo0323@gmail.com>
2021-09-28 22:30:49 +0000

    make data of carousel images and set up constructor for Carousel class.

---
+++ react-carousel-component/src/carousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { view: 1 };
+    this.moveLeft = this.moveLeft.bind(this);
+    this.moveRight = this.moveRight.bind(this);
+    this.imageId = this.imageId.bind(this);
+  }
+}
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Carousel from './carousel';
 
-const element = <h1>hello JSX!</h1>;
+const images = [
+  {
+    id: 1,
+    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/028.png'
+  },
+  {
+    id: 2,
+    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png'
+  },
+  {
+    id: 3,
+    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/041.png'
+  },
+  {
+    id: 4,
+    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/039.png'
+  },
+  {
+    id: 5,
+    url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/050.png'
+  }
+];
 
-ReactDOM.render(element, document.querySelector('#root'));
+ReactDOM.render(<Carousel images={images} />, document.querySelector('#root'));
