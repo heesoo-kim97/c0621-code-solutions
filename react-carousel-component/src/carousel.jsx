7268381f1e0e2783e7138c210d607187bf689892
heesoo-kim97 <heesoo0323@gmail.com>
2021-09-29 00:32:45 +0000

    Completed react-carousel-component.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -10,6 +10,60 @@ export default class Carousel extends React.Component {
   }
 
   componentDidMount() {
-    this.timerID = setInterval(() => this.moveRight(), 2000);
+    this.timerID = setInterval(() => this.moveRight(), 2500);
+  }
+
+  moveLeft() {
+    clearInterval(this.timerID);
+    this.timerID = setInterval(() => this.moveRight(), 2500);
+    if (this.state.view <= 1) {
+      this.setState({ view: this.props.images.length });
+    } else {
+      this.setState(prevState => ({
+        view: prevState.view - 1
+      }));
+    }
+  }
+
+  moveRight() {
+    clearInterval(this.timerID);
+    this.timerID = setInterval(() => this.moveRight(), 2500);
+    if (this.state.view >= this.props.images.length) {
+      this.setState({ view: 1 });
+    } else {
+      this.setState(prevState => ({
+        view: prevState.view + 1
+      }));
+    }
+  }
+
+  imageId(e) {
+    clearInterval(this.timerID);
+    this.timerID = setInterval(() => this.moveRight(), 2500);
+    const id = parseInt(e.target.id);
+    this.setState({ view: id });
+  }
+
+  render() {
+    const images = this.props.images;
+    return (
+      <div className="carousel" >
+        <i className="fas fa-angle-left fa-4x" onClick={this.moveLeft}></i>
+        {images.map(image => {
+          return (this.state.view === image.id && (
+            <img key={image.id} className='image' src={image.url} alt={image.id} />
+          ));
+        })}
+        <div className="progress-dot-container">
+          {images.map(image => {
+            return (this.state.view === image.id
+              ? (<i id={image.id} key={image.id} className="fas fa-circle" onClick={this.imageId}></i>)
+              : (<i id={image.id} key={image.id} className="far fa-circle" onClick={this.imageId}></i>)
+            );
+          })}
+        </div>
+        <i className="fas fa-angle-right fa-4x" onClick={this.moveRight}></i>
+      </div >
+    );
   }
 }
